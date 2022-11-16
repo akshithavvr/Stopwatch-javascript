90bb41a921c529869dcc8362000ff2558577fd67
akshithavvr <akshithavvr@gmail.com>
2022-11-16 15:16:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+
+    var seconds = 00;
+    var min = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("tens")
+    var appendMin = document.getElementById("min")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval;
+
+    buttonStart.onclick = function () {
+
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = function () {
+        clearInterval(Interval);
+    }
+
+
+    buttonReset.onclick = function () {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+
+
+    function startTimer() {
+        tens++;
+
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+
+        }
+
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
+        if (seconds > 59) {
+            min++
+            appendMin.innerHTML = "0" + min;
+            seconds = 0
+            appendSeconds.innerHTML = "0" + 0;
+        }
+        if (min > 9) {
+            appendMin.innerHTML = min;
+        }
+    }
+
+
+}
